0480550ced7bf913360e9748f26636858075a741
Connor Miotk <connor.miotk1996@icloud.com>
2018-10-03 15:28:26 +0100

    feature: using es6 within script. Converting to functions

--- js/script.js
+++ js/script.js
@@ -1,16 +1,46 @@
+/**
+ * Only run Javascript functions when the page has loaded.
+ */
+document.addEventListener('DOMContentLoaded', () => {
+    removeBodyLoadingAnimation();
+    addLinkEventListeners();
+});
 
-//Animations
-const links = document.querySelectorAll('.link');
-links.forEach(link => link.addEventListener('mouseenter', addAnimation));
-links.forEach(link => link.addEventListener('animationend', removeAnimation));
-function addAnimation(e){
-  this.classList.add('animated','tada');
-};
-function removeAnimation(e){
-  this.classList.remove('animated','tada');
-};
-const body = document.querySelector('body');
-body.addEventListener('animationend', removeLoadIn);
-function removeLoadIn(e){
-  this.classList.remove('animated','fadeIn');
+/**
+ * Functions
+ */
+function addLinkEventListeners() {
+    const links = document.querySelectorAll('.link');
+
+    links.forEach((link) => {
+        link.addEventListener('mouseenter', () => { addAnimationClasses(link) });
+        link.addEventListener('animationend', () => { removeAnimationClasses(link) });
+    });
+}
+
+/**
+ * Remove the body loading animations
+ */
+function removeBodyLoadingAnimation() {
+    const body = document.querySelector('body');
+
+    body.classList.remove('animated', 'tada');
+}
+
+/**
+ * Add an animation
+ *
+ * @param element
+ */
+function addAnimationClasses(element){
+    element.classList.add('animated','tada');
+}
+
+/**
+ * Remove an animation
+ *
+ * @param element
+ */
+function removeAnimationClasses(element){
+  element.classList.remove('animated','tada');
 }

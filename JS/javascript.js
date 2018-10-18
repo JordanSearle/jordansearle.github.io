0ff57b0c833d80ea36430aedd9c51b489f9ee116
Jordan Searle <32457721+JordanSearle@users.noreply.github.com>
2018-10-18 14:56:05 +0100

    Add files via upload

--- JS/javascript.js
+++ JS/javascript.js
@@ -1,4 +1,23 @@
 
+function overlayClick () {
+  document.getElementById("overDIV").addEventListener("click", addRemoveAnimation);
+  document.getElementById("overDIV").addEventListener("animationend", removeAnimationDiv);
+
+}
+function addRemoveAnimation(e){
+  this.classList.add('animated','slideOutUp','faster');
+  document.body.classList.remove('bodyOverflow');
+};
+function removeAnimationDiv(e){
+  this.classList.remove('animated','slideOutUp','faster');
+  document.getElementById('overDIV').style.display = "none";
+};
+$(".hover").mouseleave(
+  function() {
+    $(this).removeClass("hover");
+  }
+);
+
 //Animations
 const links = document.querySelectorAll('.link');
 links.forEach(link => link.addEventListener('mouseenter', addAnimation));
@@ -9,8 +28,20 @@ function addAnimation(e){
 function removeAnimation(e){
   this.classList.remove('animated','tada');
 };
-const body = document.querySelector('body');
-body.addEventListener('animationend', removeLoadIn);
-function removeLoadIn(e){
-  this.classList.remove('animated','fadeIn');
-}
+//const body = document.querySelector('body');
+//body.addEventListener('animationend', removeLoadIn);
+//function removeLoadIn(e){
+  //this.classList.remove('animated','fadeIn');
+//}
+//Twitter Fetcher
+var configProfile = {
+  "profile": {"screenName": 'Kernow_jordan'},
+  "id": '282752713',
+  "domId": 'myTwitter',
+  "maxTweets": 5,
+  "enableLinks": true,
+  "showUser": true,
+  "showTime": true,
+  "showImages": false,
+};
+twitterFetcher.fetch(configProfile);
